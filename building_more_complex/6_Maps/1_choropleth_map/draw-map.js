827d2aad0f1e092fdbb5c6432b79546cfa3874ba
Mohed <mccdama@gmail.com>
2021-07-03 13:01:20 +0200

    canvas&scale

--- building_more_complex/6_Maps/1_choropleth_map/draw-map.js
+++ building_more_complex/6_Maps/1_choropleth_map/draw-map.js
@@ -11,10 +11,9 @@ async function drawMap() {
 
   const metric = "Population growth (annual %)";
   let metricDataByCountry = {};
-  dataset.forEach((element) => {
-    if (element["Series Name"] != metric)
-      return (metricDataByCountry[element["Country Code"]] =
-        +element["2017 [YR2017]"] || 0);
+  dataset.forEach((d) => {
+    if (d["Series Name"] != metric) return;
+    metricDataByCountry[d["Country Code"]] = +d["2017 [YR2017]"] || 0;
   });
   console.log(metricDataByCountry);
 
@@ -30,5 +29,55 @@ async function drawMap() {
   dimensions.boundedWidth =
     dimensions.width - dimensions.margin.left - dimensions.margin.right;
   console.log(dimensions.width);
+
+  /* plot te hole earth to cover the full bounded width, then measure the height. */
+  /* define the globe using mock-GeoJSON format. */
+
+  const sphere = { type: "Sphere" };
+
+  /* use the d3-geo / d3-geo-projecion functions to convert from lat, lng coords to x, y pixel coords. --> THE SCALE */
+  const projection = d3
+    .geoEqualEarth()
+    /* now the size should be the same as the bounds
+     * update the proj. widths --> fitWidth( , )
+     */ .fitWidth(dimensions.boundedWidth, sphere);
+
+  /* Hieght */
+  const pathGenerator = d3.geoPath(projection);
+  console.log(pathGenerator(sphere)); // similar to line generator 'd3.line()'
+
+  /* TALL using .bounds() */
+  console.log(pathGenerator.bounds(sphere));
+  /* use ES6 to grab the y1 vaalue */
+  const [[x0, y0], [x1, y1]] = pathGenerator.bounds(sphere);
+  /* we want the entire earth to fit within our bounds --> set the boundedHeight to just cover the sphere */
+  dimensions.boundedHeight = y1;
+  dimensions.height =
+    dimensions.boundedHeight + dimensions.margin.top + dimensions.margin.bottom;
+  /* Wrapper and Bound section. */
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+  console.log(wrapper);
+
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left},${dimensions.margin.top})`
+    );
+  console.log(bounds);
+
+  // create SCALE
+  /* A scale to turn the metric value (population growth amounts) into color values */
+  const metricValues = Object.values(metricDataByCountry);
+  console.log(metricValues);
+  /* extraxt small and large */
+  const metricValueExtent = d3.extent(metricValues);
+  console.log(metricValueExtent);
+
+  /* create a piecewise scales (middle)--> multiple scale in one */
 }
 drawMap();
